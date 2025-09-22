3ff675bddb8b32bcaf291704de635b75552c4721
tupm209 <tu.pm209@gmail.com>
2025-09-23 00:09:21 +0900

    Add files via upload

---
+++ hw01/hw01.js
@@ -0,0 +1,8 @@
+let a = Number(prompt("Hãy nhập vào số a"));
+let b = Number(prompt("Hãy nhập vào số b"));
+
+if (a % b === 0) {
+  alert("Số a chia hết cho b!");
+} else {
+  alert("Số a không chia hết cho b!");
+}
