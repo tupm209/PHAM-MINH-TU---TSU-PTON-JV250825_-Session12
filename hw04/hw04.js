2808f00e785dd8ec754c7cfe48a6f4c62a461fa7
tupm209 <tu.pm209@gmail.com>
2025-09-24 01:43:26 +0900

    Update hw04.js

--- hw04/hw04.js
+++ hw04/hw04.js
@@ -2,10 +2,4 @@ let a = parseInt(prompt("Hãy nhập số a!"));
 let b = parseInt(prompt("Hãy nhập số b!"));
 let c = parseInt(prompt("Hãy nhập số c!"));
 
-if (a > b && a > c) {
-  alert(`Số ${a} lớn nhất!`);
-} else if (b > a && b > c) {
-  alert(`Số ${b} lớn nhất!`);
-} else {
-  alert(`Số ${c} lớn nhất!`);
-}
+alert(`Số lớn nhất là ${Math.max(a, b, c)}`);
